76f884fc87d25b0e31b2d57c9b4190a367e81fee
BedimTim <tim.baldes@icloud.com>
2024-08-24 19:31:50 +0200

    main

--- app/api/file/route.ts
+++ app/api/file/route.ts
@@ -1,7 +1,7 @@
 import { readFile } from "fs/promises";
 import path from "path";
 
-export async function GETBeitritt() {
+export async function GET() {
     const buffer = await readFile(path.join(process.cwd(), "public/Beitritt.pdf"));
     const headers = new Headers();
     headers.append('Content-Disposition', 'attachment; filename="Beitritt.pdf"');
