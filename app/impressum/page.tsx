6422552980391d91e2dc07df4b202f7509bed9de
BedimTim <tim.baldes@icloud.com>
2024-09-20 19:31:06 +0200

    main

--- app/impressum/page.tsx
+++ app/impressum/page.tsx
@@ -22,22 +22,22 @@ export default function Home() {
             </section>
             <section className="flex-col w-full mt-8 sm:mt-4 p-8 text-white text-left bg-lime-600 bg-opacity-75 rounded-lg">
             <h2 className="text-xl pb-6">Disclaimer – rechtliche Hinweise</h2>
-§ 1 Warnhinweis zu Inhalten<br />
+            &#167; 1 Warnhinweis zu Inhalten<br />
 Die kostenlosen und frei zugänglichen Inhalte dieser Webseite wurden mit größtmöglicher Sorgfalt erstellt. Der Anbieter dieser Webseite übernimmt jedoch keine Gewähr für die Richtigkeit und Aktualität der bereitgestellten kostenlosen und frei zugänglichen journalistischen Ratgeber und Nachrichten. Namentlich gekennzeichnete Beiträge geben die Meinung des jeweiligen Autors und nicht immer die Meinung des Anbieters wieder. Allein durch den Aufruf der kostenlosen und frei zugänglichen Inhalte kommt keinerlei Vertragsverhältnis zwischen dem Nutzer und dem Anbieter zustande, insoweit fehlt es am Rechtsbindungswillen des Anbieters.
             </section>
             <section className="flex-col w-full mt-8 sm:mt-4 p-8 text-white text-left bg-lime-600 bg-opacity-75 rounded-lg">
-            <h2 className="text-xl pb-6">§ 2 Externe Links</h2>
+            <h2 className="text-xl pb-6">&#167; 2 Externe Links</h2>
             Diese Website enthält Verknüpfungen zu Websites Dritter ("externe Links"). Diese Websites unterliegen der Haftung der jeweiligen Betreiber. Der Anbieter hat bei der erstmaligen Verknüpfung der externen Links die fremden Inhalte daraufhin überprüft, ob etwaige Rechtsverstöße bestehen. Zu dem Zeitpunkt waren keine Rechtsverstöße ersichtlich. Der Anbieter hat keinerlei Einfluss auf die aktuelle und zukünftige Gestaltung und auf die Inhalte der verknüpften Seiten. Das Setzen von externen Links bedeutet nicht, dass sich der Anbieter die hinter dem Verweis oder Link liegenden Inhalte zu Eigen macht. Eine ständige Kontrolle der externen Links ist für den Anbieter ohne konkrete Hinweise auf Rechtsverstöße nicht zumutbar. Bei Kenntnis von Rechtsverstößen werden jedoch derartige externe Links unverzüglich gelöscht.
             </section>
             <section className="flex-col w-full mt-8 sm:mt-4 p-8 text-white text-left bg-lime-600 bg-opacity-75 rounded-lg">
-            <h2 className="text-xl pb-6">§ 3 Urheber- und Leistungsschutzrechte</h2>
+            <h2 className="text-xl pb-6">&#167; 3 Urheber- und Leistungsschutzrechte</h2>
             Die auf dieser Website veröffentlichten Inhalte unterliegen dem deutschen Urheber- und Leistungsschutzrecht. Jede vom deutschen Urheber- und Leistungsschutzrecht nicht zugelassene Verwertung bedarf der vorherigen schriftlichen Zustimmung des Anbieters oder jeweiligen Rechteinhabers. Dies gilt insbesondere für Vervielfältigung, Bearbeitung, Übersetzung, Einspeicherung, Verarbeitung bzw. Wiedergabe von Inhalten in Datenbanken oder anderen elektronischen Medien und Systemen. Inhalte und Rechte Dritter sind dabei als solche gekennzeichnet. Die unerlaubte Vervielfältigung oder Weitergabe einzelner Inhalte oder kompletter Seiten ist nicht gestattet und strafbar. Lediglich die Herstellung von Kopien und Downloads für den persönlichen, privaten und nicht kommerziellen Gebrauch ist erlaubt.<br />
             <br />
 Die Darstellung dieser Website in fremden Frames ist nur mit schriftlicher Erlaubnis zulässig.
 
             </section>
             <section className="flex-col w-full mt-8 sm:mt-4 p-8 text-white text-left bg-lime-600 bg-opacity-75 rounded-lg">
-            <h2 className="text-xl pb-6">§ 4 Besondere Nutzungsbedingungen</h2>
+            <h2 className="text-xl pb-6">&#167; 4 Besondere Nutzungsbedingungen</h2>
             Soweit besondere Bedingungen für einzelne Nutzungen dieser Website von den vorgenannten Paragraphen abweichen, wird an entsprechender Stelle ausdrücklich darauf hingewiesen. In diesem Falle gelten im jeweiligen Einzelfall die besonderen Nutzungsbedingungen.
             </section>
 
